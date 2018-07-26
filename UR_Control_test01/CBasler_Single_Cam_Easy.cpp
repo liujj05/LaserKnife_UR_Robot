07c1a07b951818b515aa100a7a8aa65e672871dd
liujj05 <liujj05@users.noreply.github.com>
2018-07-26 09:37:45 +0800

    完善了部分相机操作类中的函数-没有上机调试

--- UR_Control_test01/CBasler_Single_Cam_Easy.cpp
+++ UR_Control_test01/CBasler_Single_Cam_Easy.cpp
@@ -1,12 +1,100 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "CBasler_Single_Cam_Easy.h"
 
 
 CBasler_Single_Cam_Easy::CBasler_Single_Cam_Easy()
 {
+	if_init = false;
 }
 
 
 CBasler_Single_Cam_Easy::~CBasler_Single_Cam_Easy()
 {
 }
+
+bool CBasler_Single_Cam_Easy::Init_Cam(void)
+{
+	bool exitcode;
+
+	PylonInitialize();
+
+	try
+	{
+		camera.Attach(CTlFactory::GetInstance().CreateFirstDevice());
+		//camera = CInstantCamera(CTlFactory::GetInstance().CreateFirstDevice());
+#ifdef SHOW_MID_RES
+		cout << "Using device " << camera.GetDeviceInfo().GetModelName() << endl;
+#endif
+		camera.MaxNumBuffer = 5;
+		if_init = true;
+		exitcode = true;
+	}
+	catch (const GenericException &e)
+	{
+		// Error handling.
+		cerr << "An exception occurred." << endl
+			<< e.GetDescription() << endl;
+		exitcode = false;
+	}
+
+	//PylonTerminate();
+	return exitcode;
+}
+
+void CBasler_Single_Cam_Easy::Release_Cam(void)
+{
+	if_init = false;
+	PylonTerminate();
+}
+
+bool CBasler_Single_Cam_Easy::Cap_single_image(void)
+{
+	bool exitCode;
+
+	if (false == if_init)
+	{
+		cout << "Not init camera yet" << endl;
+		return false;
+	}
+
+	try
+	{
+		camera.StartGrabbing(c_countOfImagesToGrab);
+		CGrabResultPtr ptrGrabResult;
+		while (camera.IsGrabbing())
+		{
+			// Wait for an image and then retrieve it. A timeout of 5000 ms is used.
+			camera.RetrieveResult(5000, ptrGrabResult, TimeoutHandling_ThrowException);
+
+			// Image grabbed successfully?
+			if (ptrGrabResult->GrabSucceeded())
+			{
+				// Access the image data.
+#ifdef SHOW_MID_RES
+				cout << "SizeX: " << ptrGrabResult->GetWidth() << endl;
+				cout << "SizeY: " << ptrGrabResult->GetHeight() << endl;
+#endif
+				const uint8_t *pImageBuffer = (uint8_t *)ptrGrabResult->GetBuffer();
+#ifdef SHOW_MID_RES
+				cout << "Gray value of first pixel: " << (uint32_t)pImageBuffer[0] << endl << endl;
+#endif
+
+				exitCode = true;
+			}// if (ptrGrabResult->GrabSucceeded())
+			else
+			{
+				cout << "Error: " << ptrGrabResult->GetErrorCode() << " " << ptrGrabResult->GetErrorDescription() << endl;
+				exitCode = false;
+			}
+		}// while(camera.IsGrabbing())
+	}
+	catch (const GenericException &e)
+	{
+		// Error handling.
+		cerr << "An exception occurred." << endl
+			<< e.GetDescription() << endl;
+		exitCode = false;
+	}
+	return exitCode;
+}
+
--- UR_Control_test01/CBasler_Single_Cam_Easy.h
+++ UR_Control_test01/CBasler_Single_Cam_Easy.h
@@ -6,6 +6,7 @@
 #endif
 
 using namespace Pylon;
+using namespace std;
 
 class CBasler_Single_Cam_Easy
 {
@@ -13,7 +14,14 @@ public:
 	CBasler_Single_Cam_Easy();
 	~CBasler_Single_Cam_Easy();
 
+	CInstantCamera camera;
+
+	bool Init_Cam(void);			// 初始化单个相机
+	void Release_Cam(void);			// 释放所有Pylon的变量
+	bool Cap_single_image(void);	// 获取单帧图像
+
 private:
-	static const uint32_t c_countOfImagesToCrab = 1;
+	static const uint32_t c_countOfImagesToGrab = 1;
+	bool if_init;
 };
 
